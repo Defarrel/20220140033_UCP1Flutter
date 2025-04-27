b8ff87d6ff5847990e2b155a7308213ccc73b8b5
defarrel <Defarrel>
2025-04-27 12:26:13 +0700

    Menambahkan container untuk profile

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -11,11 +11,48 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: const Center(
-        child: Text('Home Page'),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(28),
+              color: Color.fromARGB(255, 14, 137, 55),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/icon.jpeg'),
+                    radius: 24,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Selamat Datang',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      Text(
+                        'Admin',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 120),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [Icon(Icons.exit_to_app, color: Colors.white)],
+                  ),
+                ],
+              ),
+            ),
+            
+          ],
+        ),
       ),
     );
   }
