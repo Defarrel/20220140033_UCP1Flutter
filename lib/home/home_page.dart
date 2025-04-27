1baa6b5b6838fd4f67b49f6a5847422947566d90
defarrel <Defarrel>
2025-04-27 13:23:31 +0700

    Menambahkan 3 container di home

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -45,7 +45,16 @@ class _HomePageState extends State<HomePage> {
                   SizedBox(width: 120),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.end,
-                    children: [Icon(Icons.exit_to_app, color: Colors.white)],
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.exit_to_app,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -54,14 +63,114 @@ class _HomePageState extends State<HomePage> {
               margin: EdgeInsets.all(16),
               height: 120,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
                 image: DecorationImage(
-                  image: AssetImage(
-                    'assets/images/banner.jpg',
-                  ), // Ganti dengan path gambar promo
+                  image: AssetImage('assets/images/banner.jpg'),
                   fit: BoxFit.cover,
                 ),
               ),
+            ),
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 50)),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 16, right: 8),
+                        height: 180,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 102, 185, 51),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.groups_2),
+                              color: Colors.white,
+                              iconSize: 40,
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              'Data Piket',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 180,
+                        margin: EdgeInsets.only(left: 8, right: 16),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 102, 185, 51),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.person_add_alt_1,
+                                color: Colors.white,
+                                size: 40,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              'Data Pelanggan',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Container(
+                  margin: EdgeInsets.only(left: 16, right: 16, top: 8),
+                  height: 240,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 102, 185, 51),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.inventory_2,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Barang Masuk/Keluar',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
