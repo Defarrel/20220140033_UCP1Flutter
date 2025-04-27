37de07ffcd643a560d4d1aee1e7a694b00e071dc
defarrel <Defarrel>
2025-04-27 16:38:29 +0700

    Menambahkan textformfield piket gudang

---
+++ lib/piket/piket_gudang.dart
@@ -0,0 +1,179 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class PiketGudang extends StatefulWidget {
+  const PiketGudang({super.key});
+
+  @override
+  State<PiketGudang> createState() => _PiketGudangState();
+}
+
+class _PiketGudangState extends State<PiketGudang> {
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController tanggalController = TextEditingController();
+  final TextEditingController piketController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        backgroundColor: Color.fromARGB(255, 102, 185, 51),
+        title: Text('Piket Gudang', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        toolbarHeight: 100,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Nama Anggota', style: TextStyle(fontSize: 18)),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: namaController,
+                decoration: InputDecoration(
+                  hintText: 'Nama Anggota',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 102, 185, 51),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nama tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              Text('Pilih Tanggal', style: TextStyle(fontSize: 18)),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: tanggalController,
+                decoration: InputDecoration(
+                  hintText: 'Pilih Tanggal',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 102, 185, 51),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                  prefixIcon: Icon(Icons.calendar_month),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Tanggal tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Tugas Piket', style: TextStyle(fontSize: 18)),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: piketController,
+                          decoration: InputDecoration(
+                            hintText: 'Tugas Piket',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 102, 185, 51),
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Piket tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 30),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                            backgroundColor: const Color.fromARGB(
+                              255,
+                              102,
+                              185,
+                              51,
+                            ),
+                            minimumSize: const Size(390, 55),
+                          ),
+                          child: const Text(
+                            'Simpan',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 30),
+                    Text(
+                      'Daftar Tugas Piket',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
