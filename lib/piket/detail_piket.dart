280e2795a4d41c1146f9ce3b98f15736327c24fe
defarrel <Defarrel>
2025-04-27 19:09:53 +0700

    Menambahkan Detail tanggal, nama, dan tugas

--- lib/piket/detail_piket.dart
+++ lib/piket/detail_piket.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class DetailPiket extends StatelessWidget{
+class DetailPiket extends StatelessWidget {
   final String username, tanggal, piket;
   const DetailPiket({
     super.key,
@@ -12,7 +12,7 @@ class DetailPiket extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: true,
         backgroundColor: Color.fromARGB(255, 102, 185, 51),
@@ -23,6 +23,49 @@ class DetailPiket extends StatelessWidget{
         centerTitle: true,
         toolbarHeight: 100,
         iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(35.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    tanggal,
+                    style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 102, 185, 51), fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 17, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 102, 185, 51),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text(
+                    username,
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 102, 185, 51),
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Text(
+                piket,
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
