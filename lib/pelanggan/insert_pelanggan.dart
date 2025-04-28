a47e025d0aa4b1345efccb0d574ffd677b499c25
defarrel <Defarrel>
2025-04-28 09:47:43 +0700

    Menambahkan semua TextFormField di data pelanggan

--- lib/pelanggan/insert_pelanggan.dart
+++ lib/pelanggan/insert_pelanggan.dart
@@ -16,7 +16,7 @@ class _InsertPelangganState extends State<InsertPelanggan> {
   final TextEditingController kodePosController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   @override
-    void initState() {
+  void initState() {
     super.initState();
   }
 
@@ -33,11 +33,211 @@ class _InsertPelangganState extends State<InsertPelanggan> {
         iconTheme: IconThemeData(color: Colors.white),
       ),
       body: Form(
-        child: Padding(padding: const EdgeInsets.all(15.0), child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [],
-        )),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Nama Cust', style: TextStyle(fontSize: 18)),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: namaCustomerController,
+                decoration: InputDecoration(
+                  hintText: 'Nama Cust',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 102, 185, 51),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Cust tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Email', style: TextStyle(fontSize: 18)),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            hintText: 'Email',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 102, 185, 51),
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Email tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('No HP', style: TextStyle(fontSize: 18)),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: noHpController,
+                          decoration: InputDecoration(
+                            hintText: 'No HP',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 102, 185, 51),
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'No HP tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text('Alamat', style: TextStyle(fontSize: 18)),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: alamatController,
+                decoration: InputDecoration(
+                  hintText: 'Alamat',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 102, 185, 51),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(18)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Alamat tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Provinsi', style: TextStyle(fontSize: 18)),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: provinsiController,
+                          decoration: InputDecoration(
+                            hintText: 'Provinsi',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 102, 185, 51),
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Provinsi tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Kode Pos', style: TextStyle(fontSize: 18)),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: kodePosController,
+                          decoration: InputDecoration(
+                            hintText: 'Kode Pos',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 102, 185, 51),
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(18),
+                              ),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Pos tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                        
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
